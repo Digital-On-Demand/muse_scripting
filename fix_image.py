c4cf1f35353af39901885890055b79ab2faf19b8
DODGavin <gavinb@digitalondemand.com>
2025-06-10 16:13:44 -0400

    reversion
    reverted image fixing algorithm to remove background then invert all non-black

--- fix_image.py
+++ fix_image.py
@@ -2,65 +2,48 @@ import numpy as np
 from PIL import Image
 import sys
 import os
-from scipy.ndimage import binary_dilation, label
 
 # static variables
 DO_NOTHING = False  # set to True to skip image processing
-WHITE_THRESHOLD = 253  # can be tweaked (0-255)
-BLACK_THRESHOLD = 2  # can be tweaked (0-255)
-TRANSPARENT_THRESHOLD = 250  # can be tweaked (0-255)
-
-def edge_contiguous_mask(mask):
-    from skimage.segmentation import clear_border
-    return clear_border(mask)
-
-def compress_contrast(arr, factor=0.25):
-    return np.clip((arr - 128) * factor + 128, 0, 255).astype(np.uint8)
+WHITE_THRESHOLD = 250  # can be tweaked (0-255)
+BLACK_THRESHOLD = 4  # can be tweaked (0-255)
 
 def fix_image(input_path, recipe_name, fixed_file_path):
     with Image.open(input_path) as img:
         if DO_NOTHING:
             fixed = img
         else:
-            # some image manip based on recipe
+            # flip image along x axis if rocksBottom, otherwise rotate 90 degrees CCW
             if "rocksbottom" in recipe_name.lower():
                 print("Flipping image along x axis")
                 fixed = img.transpose(Image.FLIP_LEFT_RIGHT)
-            elif "chug" in recipe_name.lower():
-                print("Rotating image 180 degrees")
-                fixed = img.rotate(180, expand=True)
             else:
                 print("Rotating image 90 degrees CCW")
                 fixed = img.rotate(90, expand=True)
 
-            # ensure image is in RGBA mode
+            # Ensure RGBA
             if fixed.mode != 'RGBA':
                 fixed = fixed.convert('RGBA')
 
-            # convert to numpy array for processing
+            # To NumPy for pixel math
             data = np.array(fixed)
             r, g, b, a = data[:,:,0], data[:,:,1], data[:,:,2], data[:,:,3]
 
-            # Build masks
-            white_like = (r > WHITE_THRESHOLD) & (g > WHITE_THRESHOLD) & (b > WHITE_THRESHOLD)
-            black_like = (r < BLACK_THRESHOLD) & (g < BLACK_THRESHOLD) & (b < BLACK_THRESHOLD)
-            transparent = a < TRANSPARENT_THRESHOLD
+            # Make white pixels transparent
+            white_mask = (r > WHITE_THRESHOLD) & (g > WHITE_THRESHOLD) & (b > WHITE_THRESHOLD)
+            data[white_mask, 3] = 0
 
-            # Find edge pixels connected to transparency using dilation
-            from scipy.ndimage import binary_dilation
+            # Create masks for selective inversion
+            black_mask = (r < BLACK_THRESHOLD) & (g < BLACK_THRESHOLD) & (b < BLACK_THRESHOLD)
+            invert_mask = ~(white_mask | black_mask)
 
-            near_transparent = binary_dilation(transparent, iterations=1)
-            edge_connected = (white_like | black_like) & near_transparent
-
-            # Invert everything except edge-connected white/black
-            invert_mask = ~edge_connected
-
+            # Invert only relevant pixels
             data[..., 0][invert_mask] = 255 - r[invert_mask]
             data[..., 1][invert_mask] = 255 - g[invert_mask]
             data[..., 2][invert_mask] = 255 - b[invert_mask]
 
             fixed = Image.fromarray(data, mode='RGBA')
-        fixed.save(fixed_file_path, dpi=(300, 300))
+        fixed.save(fixed_file_path)
 
 if __name__ == "__main__":
     path = input("Enter the folder path: ")
